12ce9bed8563d23ce8a35e0c35f3c9989528ad8d
Imagine Chiu <imagine10255@gmail.com>
2017-01-05 09:33:03 +0800

    mail 測試 改使用為 mailcatcher

--- im.sh
+++ im.sh
@@ -14,8 +14,8 @@ do
     # View
     echo -e "
 [docker-compose manage]
- 1) create centos7 + php7 + nginx + mysql + mailhog
- 2) create centos6 + php5.6 + nginx + mysql + mailhog
+ 1) create centos7 + php7 + nginx + mysql + mailcatcher
+ 2) create centos6 + php5.6 + nginx + mysql + mailcatcher
 st) stop all container
 de) delete all container
 ps) display all container
@@ -23,11 +23,11 @@ ps) display all container
     read -p "Enter your choice: " NUM
     case $NUM in
     1)
-        docker-compose up -d imdock-mysql imdock-cnp7 imdock-mailhog
+        docker-compose up -d imdock-mysql imdock-cnp7 imdock-mailcatcher
         exit;
     ;;
     2)
-        docker-compose up -d imdock-mysql imdock-cnp56 imdock-mailhog
+        docker-compose up -d imdock-mysql imdock-cnp56 imdock-mailcatcher
         exit;
     ;;
     st)
